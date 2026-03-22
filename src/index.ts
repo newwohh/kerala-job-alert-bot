b574cbe285412f22511a99a1ec5e145a38595bf7
newwoohh <hellonevo13@gmail.com>
2026-03-22 20:36:08 +0530

    feat: add comments outlining the main application startup and operational flow.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
 console.log("Running jobs alerts...");
 
+// start the app
+
+// connect DB
+
+// connect the bot
+
+// start jobs
+
+// start scrapers
+
+// save to db
+
+// send alerts
+
+// initialise the commands
+
